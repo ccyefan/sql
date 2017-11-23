59cc81259d0f3c044953fd9dba433c8d35a004e7
ccyefan <1169402001@qq.com>
2017-11-23 10:53:53 +0800

    not in

--- diff.sql
+++ diff.sql
@@ -6,6 +6,18 @@ select a from
   on A.tel = B.tel)
 where b is null;
 
-select tel from A  
- where A.tel not in 
+select tel from A
+ where A.tel not in
     (select B.tel from B)
+
+
+
+insert overwrite table tmk_aarrr_read_add_m partition(month_part=%s)
+select a.statis_month, a.serv_num, a.prov_id, a.city_id, a.channel_code, a.channel_name, a.use_cnt
+from tmk_aarrr_read_use_m a
+where a.statis_month=%s and a.serv_num not in
+   (select serv_num
+from tmk_aarrr_read_all6_m
+where statis_month=%s)
+
+
